d1cd8d326418593091ad0ee83bfdd11ad3a07a33
sd2 <sd2@doctordetox.in>
2025-10-17 17:46:16 +0530

    Auto backup at 2025-10-17 17:46:16

--- terminal_snippet.ps1
+++ terminal_snippet.ps1
@@ -9,6 +9,12 @@ function npmdev { npm run dev }
 function npmbuild { npm run build }
 function npmtest { npm test }
 # ----------------------------
+# schadcn Commands
+# ----------------------------
+function schadcntheme7 { npx shadcn@latest add sidebar-07 }
+function schadcnlogin5 { npx shadcn@latest add login-05 }
+function schadcnsignup5 { npx shadcn@latest add signup-05 }
+# ----------------------------
 # Laravel Commands
 # ----------------------------
 function lrart { php artisan @args }
