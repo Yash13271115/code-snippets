4e630b310289d10cbbd9c34515d3228b5f1d4b1f
sd2 <sd2@doctordetox.in>
2025-10-23 14:05:16 +0530

    Auto backup at 2025-10-23 14:05:15

--- commands/command_snippets.ps1
+++ commands/command_snippets.ps1
@@ -22,9 +22,7 @@ function npmi { npm install }
 # ----------------------------
 # schadcn Commands
 # ----------------------------
-function schadcntheme7 { npx shadcn@latest add sidebar-07 }
-function schadcnlogin5 { npx shadcn@latest add login-05 }
-function schadcnsignup5 { npx shadcn@latest add signup-05 }
+. "$PSScriptRoot\schadcn.ps1"
 # ----------------------------
 # Laravel Commands
 # ----------------------------
---
+++ commands/schadcn.ps1
@@ -0,0 +1,6 @@
+# ----------------------------
+# schadcn Commands
+# ----------------------------
+function schadcntheme7 { npx shadcn@latest add sidebar-07 }
+function schadcnlogin5 { npx shadcn@latest add login-05 }
+function schadcnsignup5 { npx shadcn@latest add signup-05 }
