c457b9f6887504ce601e21079a7a4d7d751cb339
sd2 <sd2@doctordetox.in>
2025-11-13 12:27:53 +0530

    Auto backup at 2025-11-13 12:27:53

--- src/js/blade-json.js
+++ src/js/blade-json.js
@@ -1,6 +1,6 @@
 let bladeJson;
 
-fetch('./html.json')
+fetch('./blade.json')
   .then(response => {
     if (!response.ok) throw new Error(`HTTP error ${response.status}`);
     return response.json();
