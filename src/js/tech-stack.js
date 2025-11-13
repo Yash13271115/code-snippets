e2d942b66eaf40cb94e2fffc325814c96604bedc
sd2 <sd2@doctordetox.in>
2025-11-13 14:40:04 +0530

    Auto backup at 2025-11-13 14:40:04

--- src/js/tech-stack.js
+++ src/js/tech-stack.js
@@ -8,9 +8,9 @@ document.addEventListener("DOMContentLoaded", () => {
         })
         .then(data => {
             gridContainer.innerHTML = data.map(item => `
-              <a href="${item.route}" class="bg-white rounded-2xl shadow-md hover:shadow-lg transition p-6 flex flex-col items-center text-center">
-                <img src="${item.icon}" alt="${item.name} Logo" class="h-16 w-16 mb-4">
-                <h2 class="font-semibold text-gray-800">${item.name}</h2>
+              <a href="${item.route}" class="bg-white rounded-xl shadow-md hover:shadow-lg transition p-6 flex flex-col items-center text-center">
+                <img src="${item.icon}" alt="${item.name} Logo" class="h-14 w-14 mb-4">
+                <h2 class="font-semibold text-sm text-gray-800">${item.name}</h2>
               </a>
             `).join("");
         })
