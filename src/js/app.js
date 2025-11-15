ab17c3437c2d3a287a43870f09511b211ef28919
sd2 <sd2@doctordetox.in>
2025-11-15 12:59:23 +0530

    Auto backup at 2025-11-15 12:59:23

--- src/js/app.js
+++ src/js/app.js
@@ -23,3 +23,9 @@ scrollUpBtn.addEventListener('click', () => {
     behavior: 'smooth'
   });
 });
+
+const baseUrl = location.href.includes("code-snippets")
+    ? `${location.origin}/code-snippets`
+    : location.origin;
+
+document.getElementById("favicon").href = `${baseUrl}/src/media/favicon.png`;
--- src/js/favicon.js
+++
@@ -1,5 +0,0 @@
-const baseUrl = location.href.includes("code-snippets")
-    ? `${location.origin}/code-snippets`
-    : location.origin;
-
-document.getElementById("favicon").href = `${baseUrl}/src/media/favicon.png`;
